88dbc34f8d9849c4c81a483cca09755462278a15
25phamhaiyen <145901112+25phamhaiyen@users.noreply.github.com>
2026-04-28 16:06:45 +0700

    fix: firebase connection

---
+++ lib/data/models/user_model.dart
@@ -0,0 +1,41 @@
+class UserModel {
+  final String id;
+  final String firstName;
+  final String lastName;
+  final String username;
+  final String email;
+  final String phone;
+
+  UserModel({
+    required this.id,
+    required this.firstName,
+    required this.lastName,
+    required this.username,
+    required this.email,
+    required this.phone,
+  });
+
+  String get fullName => '$firstName $lastName';
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      id: map['id'] as String? ?? '',
+      firstName: map['firstName'] as String? ?? '',
+      lastName: map['lastName'] as String? ?? '',
+      username: map['username'] as String? ?? '',
+      email: map['email'] as String? ?? '',
+      phone: map['phone'] as String? ?? '',
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'firstName': firstName,
+      'lastName': lastName,
+      'username': username,
+      'email': email,
+      'phone': phone,
+    };
+  }
+}
